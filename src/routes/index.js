1d1e32be66d5e6c3bc962c28f210c57e0d6524e0
thickporhulk283 <hn3206559@gmail.com>
2025-03-01 14:16:48 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,6 @@
 const siteRouter = require('./site');
-const vrRouter = require('./vr'); // Thêm router cho /vr
 
 function route(app) {
-    app.use('/vr', vrRouter); // Thêm route /vr
     app.use('/', siteRouter);
 }
 
